19ec37c17a01490d581fe0e2adf66943b442b26b
konkvi <konkvistador@yandex.ru>
2021-09-29 13:45:14 +0300

    Initial commit of Unit tests

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? Integer.MAX_VALUE : x / y;
+
+}
+
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    public void initTest() {
+        System.out.println("Starting new test");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    public void exitTest() {
+        System.out.println("Test is completed");
+    }
+
+    @Test
+    //проверяем обычное суммирование калькулятора (2+2=4)
+    public void testPlus() {
+        assertEquals(4, calculator.plus.apply(2, 2));
+    }
+
+    @Test
+    //проверяем вычитание с отрицательными числами (11-(-1) не равно 10)
+    public void testMinus() {
+        Assertions.assertNotEquals(10, calculator.minus.apply(11, -1));
+    }
+
+    @Test
+    // проверяем деление на ноль и выдачу Integer.MAX_VALUE
+    public void testDivide(){
+        Assertions.assertTrue(calculator.divide.apply(1,0) > 0);
+    }
+
+}
